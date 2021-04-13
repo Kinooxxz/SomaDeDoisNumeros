783061570dda9bb4cacf0c15a16badb6a36b81b4
Jhonata <jhonataugusto147@gmail.com>
2021-04-13 10:55:55 -0400

    adicionado novas questões

---
+++ src/Estruturas_condicionais/VerificadorNegativo.java
@@ -0,0 +1,16 @@
+package Estruturas_condicionais;
+
+import java.util.Scanner;
+
+public class VerificadorNegativo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite um numero");
+        int numero = scanner.nextInt();
+        if (numero > 0) {
+            System.out.println("o número é negativo");
+        } else{
+            System.out.println("o numero é positivo");
+        }
+    }
+}
